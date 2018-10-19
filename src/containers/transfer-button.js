ff4796cb4a4ca87af46e91aefb2033e01e6ab2af
unknown <goodyer@gmail.com>
2018-10-19 15:47:33 -0400

    added web3 integration and send transaction within handleClick function

--- src/containers/transfer-button.js
+++ src/containers/transfer-button.js
@@ -5,25 +5,49 @@ import Web3 from 'web3';
 class TransferButton extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //     web3: null
-    //   }
+    this.state = {
+        web3: null,
+        from_addr: null
+      }
 	this.handleClick = this.handleClick.bind(this)
   	}
 
-  	componentDidMount() {
+  	async componentDidMount() {
+		try {
 
+			if (this.state.web3 === null ) {
+				let web3 = new Web3(window.web3.currentProvider);
+				this.setState({ web3 })
+
+				let [ from_addr, _ ] = await web3.eth.getAccounts()
+				this.setState({ from_addr })
+			}
+			console.log("this.state:", this.state);
+
+		} catch (err) {
+      		console.log("Error finding web3.", err);
+    	}
   	}
 
 	async handleClick() { 
-
+		try {
+			console.log("this.state.web3:", this.state.web3);
+			let amount = ""+(1 / this.props.eth_price * 5)
+ 			await this.state.web3.eth.sendTransaction({
+	            from: this.state.from_addr,
+	            to: this.props.send_addr,
+	            value: this.state.web3.utils.toWei(amount , 'ether')
+        	})   
+        } catch (err) {
+      		console.log(err);
+    	}   
   	}
 
 
 	render() {
 		return (
 			<div>
-				<button type="button" onClick={this.handleClick} >Transfer $5 <br />({  1 /  this.props.eth_price * 5  } Ether)</button> 
+				<button type="button" onClick={this.handleClick} >Transfer $5 <br />({  1 / this.props.eth_price * 5  } Ether)</button> 
 			</div>
 		);
 	}
