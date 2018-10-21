98b5e9e5d1e19bc1378d0ede3f16152cd3306589
unknown <goodyer@gmail.com>
2018-10-21 17:27:30 -0400

    added first jest with ezyme tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 import App from './App';
+import BTCPrice from './components/btc';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const initialState = {
+	interval: 60,
+	btc_price: 0,
+	eth_price: 0
+}
+
+const mockStore = configureStore()
+let store, wrapper
+
+beforeEach( () => {
+	store = mockStore(initialState)
+	wrapper = shallow(<App store={store} />)
+})
+
+it('renders App without crashing', () => {
+	console.log(wrapper.debug(true))
+	expect(store).toEqual(false)
 });
+
+
+it.only('renders a btc price component', () => {
+	console.log(wrapper.debug(true))
+	console.log(wrapper.find(BTCPrice))
+
+	expect(wrapper.find(BTCPrice)).toEqual(true)
+})
