c8fd6ed36084eb9ef9681c8ed9bae3be7aade440
Justin Goodyer <justin@goodyer.ca>
2019-08-10 18:41:33 -0400

    Updates

---
+++ precache-manifest.60e8145947e6494c2d6a2e6dac69f831.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a096e11d035ed695ac1",
+    "url": "/price_watch/static/js/runtime~main.6a096e11.js"
+  },
+  {
+    "revision": "af0a714f397dde47da16",
+    "url": "/price_watch/static/js/main.af0a714f.chunk.js"
+  },
+  {
+    "revision": "d6f9b89fee81948028a1",
+    "url": "/price_watch/static/js/1.d6f9b89f.chunk.js"
+  },
+  {
+    "revision": "af0a714f397dde47da16",
+    "url": "/price_watch/static/css/main.ac50d0a3.chunk.css"
+  },
+  {
+    "revision": "bd3b2e19773a76f3cc12b8c420bf076a",
+    "url": "/price_watch/index.html"
+  }
+];
